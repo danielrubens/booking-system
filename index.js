856562ad610b0d8b687a23daf77d59923a709905
danielrubens <danielrubensdaniel@gmail.com>
2022-12-12 20:35:05 -0300

    setup mongodb and  sudo lsof -t -i:8800

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
 const app = express()
 
-app.listen(8800, () => console.log("Connected to backend"))
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO)
+        console.log("Connected to mongoDB")
+    } catch (error) {
+        console.log({ mongose: error })
+    }
+}
+
+// mongoose.connection.on("disconnect" () => {
+//     console.log("")
+// })
+app.listen(8800, () =>{
+    connect()
+    console.log("Connected to backend")
+})
+    
