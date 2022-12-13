26eb4e15441522e4f24abd0be36a52b903ab8b1b
danielrubens <danielrubensdaniel@gmail.com>
2022-12-12 21:25:04 -0300

    Hotel model

---
+++ api/models/Hotel.js
@@ -0,0 +1,49 @@
+import { Schema } from "mongoose";
+
+const hotel = new Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    type:{
+        type: String,
+        required: true,
+    },
+    city:{
+        type: String,
+        required: true,
+    },
+    address:{
+        type: String,
+        required: true,
+    },
+    distance:{
+        type: String,
+        required: true,
+    },
+    photos:{
+        type: [String],
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    rating:{
+        type: Number,
+        min: 0,
+        max: 5
+    },
+    rooms:{
+        type: [String],
+        required: true,
+    },
+    cheapestPrice:{
+        type: Number,
+        required: true,
+    },
+    featured:{
+        type: Boolean,
+        default: false,
+    },
+
+})
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -2,6 +2,9 @@ import { Router } from "express";
 
 const router = Router()
 
+router.post('/', (req, res) => {
+    
+})
 router.get('/', (req, res) => {
     res.send("Hello, this is an auth endpoint")
 })
