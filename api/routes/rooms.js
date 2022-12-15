14d88e4eb85c04d29037fcb6b9fe6f5b60aec052
danielrubens <danielrubensdaniel@gmail.com>
2022-12-15 00:08:49 -0300

    app.post(/rooms)

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import Authorize from '../utils/Authorize.js'
+import controller from '../controllers/room.js'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.send("Hello, this is an auth endpoint")
-})
+router.get('/', controller.getAll)
+router.get('/:id', controller.getById)
+router.post('/:hotelid', Authorize.admin, controller.insert)
+router.delete('/:id', Authorize.admin, controller.remove)
+router.put('/:id', Authorize.admin, controller.update)
 
 export default router
