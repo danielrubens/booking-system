584478a3197e1fb295e886696ba2b70f35a376d2
danielrubens <danielrubensdaniel@gmail.com>
2022-12-16 01:37:04 -0300

    proxy on package.json

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,6 +1,10 @@
 import "./featured.css";
+import useFetch from "../../hooks/useFetch";
 
 const Featured = () => {
+
+  const { data, loading, error } = useFetch("")
+
   return (
     <div className="featured">
       <div className="featuredItem">
